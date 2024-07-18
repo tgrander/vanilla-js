75945a47ce3b797d045f7171696e63dbb9b7d239
Trey Granderson <treygranderson@gmail.com>
2024-07-18 04:00:58 -0400

    tsconfig: import path alias

---
+++ src/utils/loadCSS.ts
@@ -0,0 +1,7 @@
+export function loadCSS(filename: string) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = filename;
+  document.head.appendChild(link);
+}
